3914ef08425083c5421fe24fdc5b7236eefd570b
Yogadimas <yogadimaspambudi@gmail.com>
2023-08-05 21:42:22 +0700

    DOM Selection - node root - mengubah node root

---
+++ _3_4_DOM_Selection/c_mengubah_node_root/script.js
@@ -0,0 +1,10 @@
+
+// mengubah node root menjadi lebih spesifik
+const sectionB = document.getElementById("b");
+// const sectionB = document.querySelector("section#b");
+
+// menggunakan node root menjadi lebih spesifik
+const p4 = sectionB.querySelector("p");
+// const p4 = sectionB.getElementsByTagName("p")[0];
+
+p4.style.backgroundColor = "orange";
